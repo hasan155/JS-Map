68ff13317ad84b0c9dc80ac0bcc7e10e3666e477
Hasan Mahmud <hasanmahmud548589@gmail.com>
2019-08-04 10:42:21 +0600

    Javascript Map()

---
+++ Hasan.js
@@ -0,0 +1,111 @@
+// const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const filterNuber = number.filter(n => n >= 0);
+
+// const items = filterNuber.map(n => {
+//     const obj = {
+//         value: n
+//     }
+//     return obj;
+// });
+
+// const items = number
+//     .filter(n => n >= 0)
+//     .map(n => ({value: n}))
+//     .filter(obj => obj.value > 1)
+//     .map(obj => obj.value);
+
+// console.log(items);
+
+///////////////////////////// Again //////////////////////////////////
+
+
+const totalNumbers = [1, 2, 3, 4, 5, 6];
+
+const number = totalNumbers.filter(n => n >= 0);
+
+// const items = number.map(n => '<h1>'+ n +'</h1>');
+
+const items = number.map(n => {
+    return {
+        Hasan: n
+    }
+})
+
+
+console.log(items);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
